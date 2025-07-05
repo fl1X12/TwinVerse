16c43a46cca2889d51f8891bf1bdecc3508a2ac5
Jv Shanmuga Teja <119280156+fl1X12@users.noreply.github.com>
2025-07-05 17:34:01 +0530

    post models

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class verse_post(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    caption=models.TextField(max_length=150)
+    image=models.ImageField(upload_to='verse_images/')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
